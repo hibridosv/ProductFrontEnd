033b6fef648974788cd84f53ac52c15fcceec8bc
aericknunez <aerick.nunez@gmail.com>
2023-05-26 02:38:01 -0600

    Fixed api route

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         REACT_APP_NAME: 'Aplicación react',
-        REACT_APP_API_URL: 'https://connect.test/api/',
+        REACT_APP_API_URL: 'https://products.latam-pos.com/api/',
       },
 }
 
