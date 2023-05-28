3e38f0651adad6cf73f8387a2ed172338b7bd318
aericknunez <aerick.nunez@gmail.com>
2023-05-28 13:21:57 -0600

    Git Ignore

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        REACT_APP_NAME: 'Aplicación react',
+        REACT_APP_API_URL: 'http://products.latam-pos.com/api/',
+      },
+}
+
+module.exports = nextConfig
