35973fca17805cb5e97a078f95cf0b7c8a3aabf0
aericknunez <aerick.nunez@gmail.com>
2023-05-28 23:57:31 -0600

    fixed untracked files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        REACT_APP_NAME: 'Aplicación react',
-        REACT_APP_API_URL: 'https://products.latam-pos.com/api/',
-      },
-}
-
-module.exports = nextConfig
