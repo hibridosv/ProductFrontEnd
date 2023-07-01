421268b0eb652cf041470aa3633c447dfd81f9f9
aericknunez <aerick.nunez@gmail.com>
2023-07-01 01:52:15 -0600

    Fix url

--- src/app/product/edit/page.tsx
+++ src/app/product/edit/page.tsx
@@ -33,7 +33,7 @@ export default function EditPage() {
     }, [searchTerm]);
 
     const listItems = products?.map((product: any):any => (
-      <Link key={product.id} href={`product/edit/${product.id}`}>
+      <Link key={product.id} href={`/product/edit/${product.id}`}>
       <li className="flex justify-between p-3 hover:bg-blue-200 hover:text-blue-800">
       {product.cod} | {product.description}
           <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24"
