e79e640bc41cbe29eebf296230f6c6625a55911b
aericknunez <42759691+aericknunez@users.noreply.github.com>
2023-11-11 04:30:41 -0600

    Update

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 export const NAME = process.env.REACT_APP_NAME || 'React App';
-// const isLocalhost = typeof window !== 'undefined' && window.location.href === "http://localhost:3000/";
-// export const URL = isLocalhost ? "http://connect.test/" : "https://products.latam-pos.com/";
-// export const API_URL = `${URL}api/`;
-export const API_URL = `http://connect.test/api/`;
+const isLocalhost = typeof window !== 'undefined' && window.location.href === "http://localhost:3000/";
+export const URL = isLocalhost ? "http://connect.test/" : "https://products.latam-pos.com/";
+export const API_URL = `${URL}api/`;
+// export const URL = `http://connect.test/`;
+// export const API_URL = `http://connect.test/api/`;
