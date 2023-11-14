2b4417dd23d3fcce17e594e801c7cc0c7f02e6af
aericknunez <42759691+aericknunez@users.noreply.github.com>
2023-11-14 01:53:54 -0600

    Fixed some bugs

--- src/components/cash/cash-show-total.tsx
+++ src/components/cash/cash-show-total.tsx
@@ -15,7 +15,7 @@ export function CashShowTotal(props: CashShowTotalProps) {
 
   const texStyle = isSending ? "flex justify-center text-3xl mb-4 text-gray-500 animate-pulse" : "flex justify-center text-3xl mb-4"; 
   const listItems = records.data.map((record: any) => (
-    <div className="w-full my-4 shadow-neutral-600 shadow-lg rounded-md">
+    <div key={record.id} className="w-full my-4 shadow-neutral-600 shadow-lg rounded-md">
       <div className="flex justify-center pt-2 font-bold">{record?.bank}</div>
       <div className={`${texStyle} pb-4 font-bold`}>{numberToMoney(record?.balance)}</div>
     </div>
