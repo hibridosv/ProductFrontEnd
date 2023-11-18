108250b6d9c9f1bcfe3df40ff4dd79a9836a664d
aericknunez <42759691+aericknunez@users.noreply.github.com>
2023-11-18 02:26:43 -0600

    Fixed click

--- src/app/histories/page.tsx
+++ src/app/histories/page.tsx
@@ -6,7 +6,7 @@ import { DateRange, DateRangeValues } from "@/components/form/date-range"
 
 export default function Page() {
 
-    
+
     const handleFormSubmit = async (values: DateRangeValues) => {
         console.log(values)
       };
@@ -16,7 +16,7 @@ export default function Page() {
     <div className="grid grid-cols-1 md:grid-cols-10 pb-10">
         <div className="col-span-7 border-r md:border-sky-600">
             <div className="flex justify-between">
-            <span className=" m-4 text-2xl "><Button preset={Preset.add} text="AGREGAR" onClick={()=>setIsAdContactModal(true)} /></span>
+            <span className=" m-4 text-2xl "><Button preset={Preset.add} text="AGREGAR" onClick={()=>console.log(true)} /></span>
             </div>
 
 
