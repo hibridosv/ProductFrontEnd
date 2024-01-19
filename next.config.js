28124838a752a85c8913f2c8e78a20d0a4d95ddf
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-01-19 09:02:04 -0600

    Change config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
     // output: 'export',
     env: {
         REACT_APP_NAME: 'Aplicación react',
-        REACT_APP_API_URL: 'http://connect.test/',
-        CLIENT_ID: '9b213143-8316-4215-89df-de9e17601fe9',
-        CLIENT_SECRET: 'mM2mZBPvAdKncX54jQvGvK4JaVQmcKlD66YVBqve',
-        // REACT_APP_API_URL: 'https://products.latam-pos.com/',
-        // CLIENT_ID: '9b2130f5-4465-4ac7-9e6f-2a1d08471be0',
-        // CLIENT_SECRET: 'yYUu6rsCz7Biu8cVOafAofylfYNIYISoQINOIB6N',
+        // REACT_APP_API_URL: 'http://connect.test/',
+        // CLIENT_ID: '9b213143-8316-4215-89df-de9e17601fe9',
+        // CLIENT_SECRET: 'mM2mZBPvAdKncX54jQvGvK4JaVQmcKlD66YVBqve',
+        REACT_APP_API_URL: 'https://products.latam-pos.com/',
+        CLIENT_ID: '9b2130f5-4465-4ac7-9e6f-2a1d08471be0',
+        CLIENT_SECRET: 'yYUu6rsCz7Biu8cVOafAofylfYNIYISoQINOIB6N',
       },
       images: {
         remotePatterns: [
