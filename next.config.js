948a805fe796b8bb30bf6de0197030537f25a15d
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-01-28 21:18:22 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
     env: {
         REACT_APP_NAME: 'Aplicación react',
         REACT_APP_API_URL: 'http://billing.test/',
-        CLIENT_ID: '9b21e9e5-1a36-42f1-811f-4bc71b1b548e',
-        CLIENT_SECRET: 'kkUuzbaQCYKfjao9dugEVvl20PE6Rhp0QLLzMtJv',
+        CLIENT_ID: '9b347d35-75b0-42ba-8f6f-1d74155f3270',
+        CLIENT_SECRET: 'c47LQcXrNDpJrqHSnYGJMoOwM7WnINL76vN241KB',
         // REACT_APP_API_URL: 'https://products.latam-pos.com/',
         // CLIENT_ID: '9b2130f5-4465-4ac7-9e6f-2a1d08471be0',
         // CLIENT_SECRET: 'yYUu6rsCz7Biu8cVOafAofylfYNIYISoQINOIB6N',
