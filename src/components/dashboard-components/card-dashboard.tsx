a726abc0eacf2f270ff3804de84ab8fdda543eed
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-02-28 07:36:24 -0600

    Shadows card

--- src/components/dashboard-components/card-dashboard.tsx
+++ src/components/dashboard-components/card-dashboard.tsx
@@ -17,16 +17,16 @@ export function CardDashBoard(props: CardDashBoardProps){
 
   const selectTheme = (theme: number): string =>{
     switch (theme) {
-      case 1: return "cyan"
-      case 2: return "red"
-      case 3: return "green"
-      case 4: return "blue"
-      default: return "slate"
+      case 1: return "shadow-cyan-500"
+      case 2: return "shadow-red-500"
+      case 3: return "shadow-green-500"
+      case 4: return "shadow-blue-500"
+      default: return "shadow-slate-500"
     }
   }
 
     return(
-        <div className={`mx-4 border-2 border-slate-600 shadow-md shadow-${selectTheme(records.theme)}-500 rounded-md w-full`}>
+        <div className={`mx-4 border-2 border-slate-600 shadow-md ${selectTheme(records.theme)} rounded-md w-full`}>
             <div className="w-full text-center">{records.title}</div>
             <div className="w-full text-center text-2xl my-2 font-bold">{records.isMoney ? `$ ${records.value}` : records.value}</div>
         </div>
