d3a7e27fd872062edc2d0776b0eab38c30b046ca
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-05-23 03:58:07 -0600

    Account Search

--- src/app/credits/payable/page.tsx
+++ src/app/credits/payable/page.tsx
@@ -6,8 +6,11 @@ import { CreditAddPayableModal } from "@/components/credits-components/credits-a
 import { CreditAddPaymentModal, Type } from "@/components/credits-components/credits-add-payment-modal";
 import { CredistPayableTable } from "@/components/credits-components/credits-payable-table";
 import { usePagination } from "@/hooks/usePagination";
-import { getTotalOfItem, loadData } from "@/utils/functions";
+import { getRandomInt, getTotalOfItem, loadData } from "@/utils/functions";
 import { useEffect, useState } from "react";
+import { SearchInput } from "@/components/form/search";
+import { useSearchTerm } from "@/hooks/useSearchTerm";
+import { getData } from "@/services/resources";
 
 export default function CreditPayablePage() {
   const [isAddPayableModal, setIsAddPayableModal] = useState(false);
@@ -18,12 +21,19 @@ export default function CreditPayablePage() {
   const [creditsTotal, setCreditsTotal] = useState(0);
   const [creditsQuantity, setCreditsQuantity] = useState(0);
 
+  const [ randNumber, setrandNumber] = useState(0) as any;
+  const { searchTerm, handleSearchTerm } = useSearchTerm(["name", "id_number"], 500);
+  const [contacts, setContacts] = useState([]) as any;
+  const [contactSelected, setContactSelected] = useState(null) as any;
+
+
+
   useEffect(() => {
     if (!isAddPayableModal && !isAddPaymentModal) {
-      (async () => setCredits(await loadData(`credits/payable?sort=-created_at&perPage=10${currentPage}`)))();
+      (async () => setCredits(await loadData(`credits/payable?${contactSelected && `filterWhere[provider_id]==${contactSelected.id}&`}sort=-created_at&perPage=10${currentPage}`)))();
     }
-  }, [isAddPayableModal, isAddPaymentModal, currentPage]);
-  
+  }, [isAddPayableModal, isAddPaymentModal, currentPage, contactSelected]);
+  console.log(`credits/payable?${contactSelected && `filterWhere[provider_id]==${contactSelected.id}&`}sort=-created_at&perPage=10${currentPage}`)
   useEffect(() => {
       if (credits.data && credits?.data.length > 0) {
         let dataFiltered = credits?.data.filter((element:any) => element.status === 1)
@@ -31,6 +41,51 @@ export default function CreditPayablePage() {
         setCreditsTotal(getTotalOfItem(credits?.data, "balance"))
       }
   }, [setCreditsQuantity, setCreditsTotal, credits]);
+
+
+console.log("contactSelected", contactSelected)
+
+  const handleCancelContact = () => {
+      setContactSelected(null)
+      setrandNumber(getRandomInt(100));
+      setContacts([])
+  }
+  const handleSelectContact = (contact: any) => {
+      setContactSelected(contact)
+      setrandNumber(getRandomInt(100));
+      setContacts([])
+  }
+
+  const loadDataContacts = async () => {
+      try {
+      const response = await getData(`contacts?filterWhere[is_provider]==1&sort=-created_at&perPage=10${searchTerm}`);
+      setContacts(response.data);
+      } catch (error) {
+      console.error(error);
+      }
+  };
+
+  useEffect(() => {
+      if (searchTerm) {
+          (async () => { await loadDataContacts();})();   
+      }
+      if (searchTerm == "") {
+          setContacts([]);
+      }
+      // eslint-disable-next-line
+  }, [searchTerm]);
+
+  const listItems = contacts?.map((contact: any):any => (
+    <div key={contact.id} onClick={()=>handleSelectContact(contact)}>
+        <li className="flex justify-between p-3 hover:bg-blue-200 hover:text-blue-800 cursor-pointer">
+              {contact.name} | {contact.id_number ? contact.id_number : ""}
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+            </svg>
+        </li>
+    </div>
+))
 
 
   return (
@@ -47,6 +102,29 @@ export default function CreditPayablePage() {
             <ViewTitle text="RESUMEN" />
             <CreditsShowTotal quantity={creditsQuantity} text="Creditos Pendientes" number />
             <CreditsShowTotal quantity={creditsTotal} text="Total Pendiente" />
+
+            <div className="mx-2">
+              <SearchInput handleSearchTerm={handleSearchTerm} placeholder="Buscar por proveedor" randNumber={randNumber} />
+              <div className="w-full bg-white rounded-lg shadow-lg mt-4">
+                  <ul className="divide-y-2 divide-gray-400">
+                  { listItems }
+                  { contacts && contacts.length > 0 && 
+                          <li className="flex justify-between p-3 hover:bg-red-200 hover:text-red-800 cursor-pointer" onClick={handleCancelContact}>
+                              CANCELAR
+                              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                                  stroke="currentColor">
+                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                              </svg>
+                          </li> }
+                  </ul>
+              </div>
+              { contactSelected &&
+              <div className="flex justify-between px-2 mb-3 uppercase text-lg font-semibold shadow-md rounded-md">
+                  <span>{ contactSelected?.name }</span> 
+                  <span className="text-right"><Button noText preset={Preset.smallClose} onClick={handleCancelContact} /></span>
+              </div> }
+          </div>
+          
         </div>
         <CreditAddPayableModal isShow={isAddPayableModal} onClose={()=>setIsAddPayableModal(false)} />
         <CreditAddPaymentModal isShow={isAddPaymentModal} onClose={()=>setIsAddPaymentModal(false)} accountType={Type.payable} creditSelected={isCreditSelect} />
