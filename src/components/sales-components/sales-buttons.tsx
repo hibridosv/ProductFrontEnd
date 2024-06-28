c83e3e43916b56b82bed0c08baa1ba4d21f38589
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-06-28 10:43:00 -0600

    improve class

--- src/components/sales-components/sales-buttons.tsx
+++ src/components/sales-components/sales-buttons.tsx
@@ -34,25 +34,6 @@ export function SalesButtons(props: SalesButtonsProps) {
 
   let fieldsRequired = validateFields();
 
-  const showOptions = () =>{
-    return (<div className="w-8/10">
-              { config.includes("sales-discount") && 
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.discount)}> Agregar Descuento </Dropdown.Item>}
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.client)}> Asignar Cliente </Dropdown.Item>
-              { config.includes("sales-other-seller") && 
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.seller)}> Asignar Vendedor </Dropdown.Item>}
-              { config.includes("sales-referred") && 
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.referred)}> Asignar Referido </Dropdown.Item>}
-              { config.includes("sales-delivery-man") && 
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.delivery)}> Asignar Repartidor </Dropdown.Item>}
-              { config.includes("sales-other-sales") && 
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.special)}> Venta Especial </Dropdown.Item>}
-              { config.includes("sales-comment") && 
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.comment)}> Agregar comentario </Dropdown.Item>}
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.documentType)}> Tipo de Documento </Dropdown.Item>
-              <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.quotes)}> Guardar como Cotización </Dropdown.Item>
-   </div>)
-   }
 
   return (<div>
           { !cashDrawer && <Alert
@@ -98,7 +79,25 @@ export function SalesButtons(props: SalesButtonsProps) {
               <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.documentType)}> Tipo de Documento </Dropdown.Item>
               <Dropdown.Item onClick={()=>onClick(OptionsClickOrder.quotes)}> Guardar como Cotización </Dropdown.Item>
             </Dropdown> */}
-            <Tooltip animation="duration-300" content={showOptions()} style="light" >
+            <Tooltip animation="duration-300" content={
+                <div className="w-8/10">
+                { config.includes("sales-discount") && 
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.discount)}>  Agregar Descuento</div>}
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.client)}> Asignar Cliente</div>
+                { config.includes("sales-other-seller") && 
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.seller)}>Asignar Vendedor</div>}
+                { config.includes("sales-referred") && 
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.referred)}> Asignar Referido</div>}
+                { config.includes("sales-delivery-man") && 
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.delivery)}> Asignar Repartidor </div>}
+                { config.includes("sales-other-sales") && 
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.special)}> Venta Especial</div>}
+                { config.includes("sales-comment") && 
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.comment)}> Agregar comentario </div>}
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.documentType)}> Tipo de Documento </div>
+                <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>onClick(OptionsClickOrder.quotes)}> Guardar como Cotización</div>
+                </div>
+              } style="light" >
                 <div className='button-left-grey clickeable'><IoMdOptions className='mr-1' /> Opciones</div>
             </Tooltip>
             
