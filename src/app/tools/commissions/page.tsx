250f97111d7f58f9c3de20b51f62ead68d9f1ee2
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-07-05 14:36:34 -0600

    Fix Nomber

--- src/app/tools/commissions/page.tsx
+++ src/app/tools/commissions/page.tsx
@@ -220,7 +220,7 @@ export default function Page() {
                   autoclass={false}
                   divider="&">
                             <li className="flex justify-between p-3 hover:bg-blue-200 hover:text-blue-800 cursor-pointer">
-                                DESRCARGAR REPORTE EXCEL
+                                DESCARGAR REPORTE EXCEL
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24"
                                     stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
