287f4dea7fdca449914d36e9386a6abd4fbdb8dd
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-07-08 01:49:48 -0600

    Update invoice-documents-electronic-table.tsx

--- src/components/invoice-components/invoice-documents-electronic-table.tsx
+++ src/components/invoice-components/invoice-documents-electronic-table.tsx
@@ -47,10 +47,10 @@ const tipoDTE = (dte: string)=>{
       <td className={`py-2 px-6 ${record?.status == 4 ? 'clickeable font-semibold' : 'text-red-500'}`}>
         { record?.status == 4 ?
         <a target="_blank" href={`${API_URL}documents/download/${record?.codigo_generacion}/${record?.client_id}`}>{ tipoDTE(record?.tipo_dte) }</a>  :
-        <div title={record?.observaciones} onClick={()=>{ setRecordSelect(record?.codigo_generacion); setShowInvoiceModal(true)}}>{ tipoDTE(record?.tipo_dte) }</div>
+        <div title={record?.observaciones}>{ tipoDTE(record?.tipo_dte) }</div>
         }
       </td>
-      <td className="py-2 px-6">{ record?.numero_control }</td>
+      <td className="py-2 px-6" onClick={()=>{ setRecordSelect(record?.codigo_generacion); setShowInvoiceModal(true)}}>{ record?.numero_control }</td>
       <td className="py-2 px-6" title={record?.descripcion_msg}>{ status(record?.status, record?.codigo_generacion) }</td>
       <td className="py-2 px-6">{ record?.email == 1 ? "Enviado" : "Sin Enviar" }</td>
     </tr>
