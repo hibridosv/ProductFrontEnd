34e526e048a3fa89b4bcba6d8f892db45bb94e12
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-07-12 07:21:31 -0600

    env type

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,5 +1,13 @@
 'use client'
 
+const env = (enviroment: any): string => {
+    switch (enviroment) {
+        case 0: return "http://billing.test/"
+        case 1: return "https://api.latam-pos.com/"
+        case 2: return "https://apitest.latam-pos.com/"
+        default: return "https://api.latam-pos.com/"
+    }
+}
 export const NAME = process.env.REACT_APP_NAME || 'Sistema de Facturación';
-export const URL = process.env.REACT_APP_API_URL || "https://api.latam-pos.com/"
+export const URL = env(process.env.ENV_TYPE)
 export const API_URL = `${URL}api/`;
