058a914cae637d79cb9f2e22bf834b888ce7d6ad
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-07-17 02:41:17 -0600

    Update invoice-documents-electronic-table.tsx

--- src/components/invoice-components/invoice-documents-electronic-table.tsx
+++ src/components/invoice-components/invoice-documents-electronic-table.tsx
@@ -63,13 +63,13 @@ const tipoDTE = (dte: string)=>{
             <div className="w-8/10">
               <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable' onClick={()=>{ setRecordSelect(record?.codigo_generacion); setShowInvoiceModal(true)}}>Detalles del documento</div>
               
-              <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable'>
+              <div className={`w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 ${record?.status == 4 ? 'clickeable' : ''}`}>
               { record?.status == 4 ?
               <a target="_blank" href={`${API_URL}documents/download/pdf/${record?.codigo_generacion}/${record?.client_id}`} title="Descargar PDF"> Descargar PDF </a>  :
               <div> Descargar PDF</div>
               }
               </div>
-              <div className='w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 clickeable'>
+              <div className={`w-full font-semibold text-slate-700 py-2 px-4 hover:bg-slate-100 ${record?.status == 4 ? 'clickeable' : ''}`}>
               { record?.status == 4 ?
               <a target="_blank" href={`${API_URL}documents/download/json/${record?.codigo_generacion}/${record?.client_id}`} title="Descargar PDF"> Descargar JSON </a>  :
               <div> Descargar JSON</div>
