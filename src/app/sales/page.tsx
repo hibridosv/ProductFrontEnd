5abb5a5302d427e27d3e68c7f561efb18d441a00
Erick Nuñez <aerick.nunez@gmail.com>
2024-08-20 22:51:11 -0600

    Update page.tsx

--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -1,19 +1,23 @@
 'use client'
 import { ConfigContext } from '@/contexts/config-context';
 import { useRouter } from 'next/navigation';
-import { useContext, useMemo } from 'react';
+import { useContext, useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
   const { systemInformation } = useContext(ConfigContext);
 
-  const tenantSystem = useMemo(() => systemInformation?.system?.tenant?.system, [systemInformation]);
 
-  if (tenantSystem === 2 || tenantSystem === 4) {
-    router.push("/sales/orders");
-  } else {
-    router.push("/sales/quick");
-  }
+  useEffect(() => {
+    const { system } = systemInformation || {};
+    const tenantSystem = system?.tenant?.system;
+      if (tenantSystem === 2 || tenantSystem === 4) {
+        router.push("/sales/orders");
+      } else {
+        router.push("/sales/quick");
+      }
 
-  return <div></div>;
+  }, [router, systemInformation]);
+  
+  return (<div></div>)
 }
