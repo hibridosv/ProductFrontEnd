3a71c942cfd83849019353c585f9a260405f2404
Erick Nuñez <aerick.nunez@gmail.com>
2024-08-24 11:12:55 -0600

    Update pusher.js

--- src/utils/pusher.js
+++ src/utils/pusher.js
@@ -2,8 +2,12 @@ import Pusher from 'pusher-js';
 
 Pusher.logToConsole = true;
 // Reemplaza con tus credenciales de Pusher
-const pusher = new Pusher('67ef4909138ad18120e1', {
-  cluster: 'us2'
+//const pusher = new Pusher('67ef4909138ad18120e1', {
+//  cluster: 'us2'
+//});
+
+const pusher = new Pusher('746657009722e09911c3', {
+  cluster: 'mt1'
 });
 
 export default pusher;
