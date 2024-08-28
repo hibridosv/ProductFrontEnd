ad7a273dc72bd671d17787a68c44dac23b85709f
Erick Nuñez <aerick.nunez@gmail.com>
2024-08-28 00:31:06 -0600

    Update pusher.js

--- src/utils/pusher.js
+++ src/utils/pusher.js
@@ -1,8 +1,8 @@
 import Pusher from 'pusher-js';
 
-Pusher.logToConsole = true;
+// Pusher.logToConsole = true;
 // Credenciales de producccion
-//const pusher = new Pusher('67ef4909138ad18120e1', { cluster: 'us2' });
+//const pusher = new Pusher('67ef4909138ad18120e1', { cluster: 'us2' }); // para facturas
 
 const pusher = new Pusher('746657009722e09911c3', { cluster: 'mt1' });
 
