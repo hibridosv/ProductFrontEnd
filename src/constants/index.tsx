58cea67c7fe5722d299d164edf6e55e649d38432
Erick Nuñez <aerick.nunez@gmail.com>
2024-09-08 07:11:38 -0600

    Update index.tsx

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -4,5 +4,5 @@ export const NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Sistema de Ventas';
 export const URL = process.env.NEXT_PUBLIC_APP_URL || "http://connect.test/"
 export const API_URL = `${URL}api/`;
 
-export const AUTH_CLIENT = process.env.NEXT_PUBLIC_AUTH_CLIENT_ID;
-export const AUTH_SECRET = process.env.NEXT_PUBLIC_AUTH_SECRET_ID;
+export const AUTH_CLIENT = process.env.NEXT_PUBLIC_CLIENT;
+export const AUTH_SECRET = process.env.NEXT_PUBLIC_HASH;
