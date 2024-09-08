bb3016b03a8fad260039bfb8152a1d98041ad164
Erick Nuñez <aerick.nunez@gmail.com>
2024-09-08 07:23:39 -0600

    Update index.tsx

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 export const NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Sistema de Ventas';
-export const URL = process.env.NEXT_PUBLIC_APP_URL || "http://connect.test/"
+export const URL = process.env.NEXT_PUBLIC_APP_URL || "https://api.latam-pos.com/"
 export const API_URL = `${URL}api/`;
 
 export const AUTH_CLIENT = process.env.NEXT_PUBLIC_CLIENT;
