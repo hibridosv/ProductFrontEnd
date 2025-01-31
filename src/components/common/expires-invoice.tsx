301cfc7607c394f048c2d67eb332107ccfb2a968
Erick Nuñez <aerick.nunez@gmail.com>
2025-01-31 13:37:14 -0600

    Fix typo in ExpiresInvoice component message

--- src/components/common/expires-invoice.tsx
+++ src/components/common/expires-invoice.tsx
@@ -14,7 +14,7 @@ export function ExpiresInvoice(props: ExpiresInvoiceProps) {
   return (
     <div className='m-4 border border-teal-700 rounded-md shadow-md'>
         <div className="m-2">
-        Extimado cliente: Tiene <span className=" text-red-700 font-semibold">facturas pendientes de pago con {expiresDays} { expiresDays > 1 ? "dias" : "dia" } de vencido</span>. Por favor, consulte el estado de su cuenta y regularice lo antes posible para evitar inconvenientes.
+        Estimado cliente: Tiene <span className=" text-red-700 font-semibold">facturas pendientes de pago con {expiresDays} { expiresDays > 1 ? "dias" : "dia" } de vencido</span>. Por favor, consulte el estado de su cuenta y regularice lo antes posible para evitar inconvenientes.
          <Link className="text-sm button-green rounded-md m-4" href="/config/invoices" >CONSULTAR FACTURAS</Link></div>
     </div>
   );
