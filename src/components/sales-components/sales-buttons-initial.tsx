4517e3e801a77e5d43f1c21d07acb51e6b25266b
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2025-04-01 23:40:14 -0600

    Add config prop to SalesButtonsInitial and conditionally render buttons based on config

--- src/components/sales-components/sales-buttons-initial.tsx
+++ src/components/sales-components/sales-buttons-initial.tsx
@@ -5,17 +5,25 @@ import { MdMoney, MdOutlinePointOfSale } from 'react-icons/md';
 export interface SalesButtonsInitialProps {
   onClick: (option: number) => void;
   isShow?: boolean;
+  config: string[];
 }
 
 export function SalesButtonsInitial(props: SalesButtonsInitialProps) {
-  const {onClick, isShow } = props
+  const {onClick, isShow, config } = props
+  const isOtherSales = config.includes("sales-other-sales");
+  const isSpecial = config.includes("sales-special");
+
 
     if (!isShow) return <></>;
 
   return (
             <div className="flex justify-center space-x-4 mt-4 ">
-              <MdOutlinePointOfSale className="clickeable text-lime-500 shadow-md" size={24} title="Otras Ventas" onClick={()=>onClick(OptionsClickOrder.otrasVentas)} />
+              { isOtherSales && 
+              <MdOutlinePointOfSale className="clickeable text-lime-500 shadow-md" size={24} title="Otras Ventas" onClick={()=>onClick(OptionsClickOrder.otrasVentas)} /> 
+              }
+              { isSpecial && 
               <MdMoney className="clickeable text-teal-500 shadow-md" size={24} title="Ventas Especiales" onClick={()=>onClick(OptionsClickOrder.ventaSpecial)} />
+              }
             </div>
             );
 }
