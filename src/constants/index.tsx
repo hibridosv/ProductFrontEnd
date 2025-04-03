c454310396f0989d2fa217aa1c480d3ad2cd8cc0
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2025-04-02 23:40:56 -0600

    Set default values for Reverb configuration constants

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -8,7 +8,7 @@ export const AUTH_CLIENT = process.env.NEXT_PUBLIC_CLIENT;
 export const AUTH_SECRET = process.env.NEXT_PUBLIC_HASH;
 
 
-export const REVERB_HOST = process.env.NEXT_PUBLIC_REVERB_HOST;
-export const REVERB_PORT = process.env.NEXT_PUBLIC_REVERB_PORT;
-export const REVERB_SCHEME = process.env.NEXT_PUBLIC_REVERB_SCHEME;
-export const REVERB_KEY = process.env.NEXT_PUBLIC_REVERB_KEY;
+export const REVERB_HOST = process.env.NEXT_PUBLIC_REVERB_HOST || "ws.websocket.pos-sv.com";
+export const REVERB_PORT = process.env.NEXT_PUBLIC_REVERB_PORT || 443;
+export const REVERB_SCHEME = process.env.NEXT_PUBLIC_REVERB_SCHEME || "https";
+export const REVERB_KEY = process.env.NEXT_PUBLIC_REVERB_KEY || "ekuhketf21ecfqj0stxk";
