e41aa70dad7053f64c4666d91fd8240236750547
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2025-05-16 09:55:29 -0600

    Add invoice search page and invoice search table component

---
+++ src/app/invoices/search/[id]/page.tsx
@@ -0,0 +1,259 @@
+'use client'
+import { useState, useEffect, useContext } from "react";
+import { Alert, DeleteModal, Loading, ViewTitle } from "@/components";
+import { getData, postData, postForPrint } from "@/services/resources";
+import { useSearchTerm } from "@/hooks/useSearchTerm";
+import { SearchInput } from "@/components/form/search";
+import { Product } from "@/services/products";
+import toast, { Toaster } from 'react-hot-toast';
+import { Button, Preset } from "@/components/button/button";
+import { formatDateAsDMY, formatHourAsHM } from "@/utils/date-formats";
+import {  extractActiveFeature, getConfigStatus, getPaymentTypeName, getRandomInt, numberToMoney } from "@/utils/functions";
+import { FaPrint } from "react-icons/fa";
+import { RiDeleteBin2Line } from "react-icons/ri";
+import { ConfigContext } from "@/contexts/config-context";
+import { MdCreditScore, MdOutlineDelete } from "react-icons/md";
+import { API_URL } from "@/constants";
+import Link from "next/link";
+
+
+export default function Page({ params }: { params: { id: string } }) {
+    const { id } = params;
+    const [records, setRecords] = useState([]) as any;
+    const [isSending, setIsSending] = useState(false);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [showNoteModal, setShowNoteModal] = useState(false);
+    const [randNumber, setRandNumber] = useState(0);
+    const [showCodeStatus, setShowCodeStatus] = useState<boolean>(false);
+    const { config, systemInformation } = useContext(ConfigContext);
+    const [configuration, setConfiguration] = useState([] as any); // configuraciones que vienen de config
+  
+    useEffect(() => {
+      if (config?.configurations) {
+        setConfiguration(extractActiveFeature(config.configurations))
+      }
+      setShowCodeStatus(getConfigStatus("sales-show-code", config));
+      // eslint-disable-next-line
+    }, [config]);
+
+  const handleGetInvoice = async () => {
+      try {
+        setIsSending(true)
+        const response = await getData(`order?&filterWhere[id]==${id}&included=creditnotes,products,invoiceAssigned,employee,client,referred,delivery`);
+        if (response.data) {
+          setRecords(response?.data[0] ?? [])
+          // toast.success("Petición realizada correctamente");
+        } else {
+          toast.error("Faltan algunos datos importantes!");
+        }
+      } catch (error) {
+        console.error(error);
+        toast.error("Ha ocurrido un error!");
+      } finally {
+        setIsSending(false)
+      }
+    };
+      
+  
+    useEffect(() => {
+            (async () => { await handleGetInvoice() })();
+      // eslint-disable-next-line
+    }, []);
+
+ 
+  const printOrder = async () => {
+    try {
+      setIsSending(true)
+      const response = await postData(`invoices/print`, "POST", {invoice: id});
+      if (response.type === 'successful') {
+        if (configuration.includes("print-local")) {
+          await postForPrint(systemInformation?.system?.local_url_print ?? 'http://127.0.0.1/impresiones/', 'POST', response.data);
+        }
+        toast.success("Imprimiendo documento");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Ha ocurrido un error!");
+    } finally {
+      setIsSending(false)
+    }
+  };
+
+
+
+  const deleteOrder = async () => {
+    setShowDeleteModal(false)
+    try {
+      setIsSending(true)
+      const response = await postData(`invoices/delete`, "POST", {invoice: id});
+      if (response.message) {
+        await handleGetInvoice()
+        toast.success(response.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Ha ocurrido un error!");
+    } finally {
+      setIsSending(false)
+    }
+  };
+
+  const noteOrder = async () => {
+    setShowNoteModal(false)
+    try {
+      setIsSending(true)
+      const response = await postData(`invoices/credit-note`, "POST", {invoice: id});
+      if (response.message) {
+        await handleGetInvoice()
+        toast.success(response.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Ha ocurrido un error!");
+    } finally {
+      setIsSending(false)
+    }
+  };
+
+    const listProducts = records.products && records?.products.map((record: any, key: any) => (
+      <tr key={record.id} className="border-b">
+        <td className="py-2 px-6 truncate">{ record?.quantity} </td>
+        { showCodeStatus &&
+        <td className="py-2 px-6 truncate">{ record?.cod} </td>
+        }
+        <th className="py-2 px-6 text-gray-900 whitespace-nowrap dark:text-white" scope="row">{ record?.product } </th>
+        <td className="py-2 px-6">{ numberToMoney(record?.unit_price ? record?.unit_price : 0, systemInformation) }</td>
+        <td className="py-2 px-6">{ numberToMoney(record?.subtotal ? record?.subtotal : 0, systemInformation) }</td>
+        <td className="py-2 px-6">{ numberToMoney(record?.taxes ? record?.taxes : 0, systemInformation) }</td>
+        <td className="py-2 px-6">{ numberToMoney(record?.discount ? record?.discount : 0, systemInformation) }</td>
+        <td className="py-2 px-6">{ numberToMoney(record?.total ? record?.total : 0, systemInformation) }</td>
+      </tr>
+    ));
+
+
+  return (
+
+    <div className="grid grid-cols-1 md:grid-cols-4 pb-10">
+        <div className="col-span-3">
+          <ViewTitle text="BUSCAR DOCUMENTOS" />
+          {isSending ? <Loading /> :
+            <div className="mx-3 my-8 ">
+
+              <div className="grid grid-cols-4 md:grid-cols-8 gap-3 bg-white dark:bg-gray-900">
+                        <div className={`col-span-2 border-2 border-slate-600 shadow-md shadow-cyan-500 rounded-md w-full`}>
+                          <div className="w-full text-center">Cajero</div>
+                          <div className="w-full text-center text-xl my-2 font-bold">{records?.employee?.name}</div>
+                        </div>
+                        <div className={`col-span-2 border-2 border-slate-600 shadow-md shadow-cyan-500 rounded-md w-full`}>
+                          <div className="w-full text-center">Fecha</div>
+                          <div className="w-full text-center text-xl my-2 font-bold">{ formatDateAsDMY(records?.charged_at) }</div>
+                        </div>
+                        <div className={`col-span-2 border-2 border-slate-600 shadow-md shadow-cyan-500 rounded-md w-full`}>
+                          <div className="w-full text-center">Tipo</div>
+                          <div className="w-full text-center text-xl my-2 font-bold">{ records?.invoice_assigned?.name }</div>
+                        </div>
+                        <div className={`col-span-2 border-2 border-slate-600 shadow-md shadow-cyan-500 rounded-md w-full`}>
+                          <div className="w-full text-center">Pago</div>
+                          <div className="w-full text-center text-xl my-2 font-bold">{ getPaymentTypeName(records?.payment_type) }</div>
+                        </div>
+              </div>
+
+              <div className="w-full overflow-auto mt-4">
+                  <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                      <tr>
+                        <th scope="col" className="py-3 px-4 border">Cant</th>
+                        { showCodeStatus &&
+                        <th scope="col" className="py-3 px-4 border">Codigo</th>
+                        }
+                        <th scope="col" className="py-3 px-4 border">Producto</th>
+                        <th scope="col" className="py-3 px-4 border">Precio</th>
+                        <th scope="col" className="py-3 px-4 border">Subtotal</th>
+                        <th scope="col" className="py-3 px-4 border">Imp</th>
+                        <th scope="col" className="py-3 px-4 border">Descuento</th>
+                        <th scope="col" className="py-3 px-4 border">Total</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {listProducts}
+                      <tr>
+                        <th scope="col" className="py-3 px-4 border" colSpan={showCodeStatus ? 4 : 3} ></th>
+                        <th scope="col" className="py-3 px-4 border">{ numberToMoney(records?.subtotal, systemInformation) }</th>
+                        <th scope="col" className="py-3 px-4 border">{ numberToMoney(records?.taxes, systemInformation) }</th>
+                        <th scope="col" className="py-3 px-4 border">{ numberToMoney(records?.discount, systemInformation) }</th>
+                        <th scope="col" className="py-3 px-4 border">{ numberToMoney(records?.total, systemInformation) }</th>
+                      </tr>
+                    </tbody>
+                  </table>
+              </div>
+
+              <div className="uppercase shadow-lg border-x-2 ml-4 my-4 p-2">
+                {records?.employee && <div>Atendido por: <span className="font-semibold">{records?.employee?.name}</span></div>}
+                {records?.referred && <div>Nombre de referido: <span className="font-semibold">{records?.referred?.name}</span></div>}
+                {records?.client && <div>Nombre del cliente: <span className="font-semibold">{records?.client?.name}</span></div>}
+                {records?.delivery && <div>Nombre del repartidor: <span className="font-semibold">{records?.data?.delivery?.name}</span></div>}
+            </div>
+
+          {
+            records?.invoice_assigned?.type == 9 && 
+            <Alert className="m-2" text="Este Documento tiene una numeración temporal" />
+          }
+          {
+            records?.invoice_assigned?.is_electronic == 1 && 
+            <Alert className="m-2" info="Atención: " text="Este Documento se envió electronicamente" isDismisible={false}  />
+          }
+          {
+            records?.creditnotes?.length > 0 && 
+            <div>
+              <Alert className="m-2" info="Atención: " text={`Este documento contiene ${records?.creditnotes?.length} nota${records?.creditnotes?.length > 1 ? 's' : ''} de credito`} isDismisible={false}  />
+              <ul>
+                {records?.creditnotes?.map((record: any, key: any) => (
+                  <li key={key} className="flex justify-between p-3 hover:bg-blue-200 hover:text-blue-800 cursor-pointer uppercase"> 
+                  <a target="_blank" href={`${API_URL}documents/download/pdf/${record?.id}/${systemInformation?.system?.client?.id}`} title="Descargar PDF">
+                    {record?.id} | {record?.invoice} | { formatDateAsDMY(record?.emited_at) } { formatHourAsHM(record?.emited_at) }
+                  </a> 
+                  </li>
+                ))}
+              </ul>
+              
+            </div>
+          }
+            </div>
+          }
+        </div>
+        <div>
+          <ViewTitle text="OPCIONES" />
+          <div className="mt-4">
+
+            <div className="m-3 flex justify-between mb-8">
+              <div title="Imprimir"><FaPrint className="clickeable" size={45} color="blue" onClick={()=>printOrder()} /></div>
+              {
+                (records?.invoice_assigned?.type == 3 || records?.invoice_assigned?.type == 2) &&
+                <div title="Crear nota de credito"><MdCreditScore className="clickeable" size={45} color="#2F81B9" 
+                  onClick={records?.status == 3 ? ()=>setShowNoteModal(true) : ()=>toast.error("Este documento ya se encuentra eliminado")} /></div>
+              }
+
+              <div title="Eliminar orden"><RiDeleteBin2Line className="clickeable" size={45} color="red" 
+              onClick={records?.status == 3 ? ()=>setShowDeleteModal(true) : ()=>toast.error("Este documento ya se encuentra eliminado")} /></div>
+            </div>
+            
+            <Link href={`/invoices/search`}>
+              <Button text='REGRESAR' isFull type="submit" preset={Preset.cancel} />
+            </Link>
+            {
+              records?.status == 4 && <div className="mt-4"><Alert info="Atención: " text="Este documento se encuentra eliminado" isDismisible={false}  /></div>
+            }
+          </div>
+        </div> 
+        <DeleteModal isShow={showDeleteModal}
+          text="¿Estas seguro de anular este documento?"
+          onDelete={()=>deleteOrder()} 
+          onClose={()=>setShowDeleteModal(false)} />
+        <DeleteModal isShow={showNoteModal}
+          text="¿Estas seguro desea crear una nota de credito de este documento?"
+          onDelete={()=>noteOrder()} 
+          onClose={()=>setShowNoteModal(false)} />
+      <Toaster position="top-right" reverseOrder={false} />
+    </div>
+  );
+}
---
+++ src/components/invoice-components/invoice-search-table.tsx
@@ -0,0 +1,65 @@
+'use client'
+import { getPaymentTypeName, getTotalOfItem, numberToMoney } from "@/utils/functions";
+import { NothingHere } from "../nothing-here/nothing-here";
+import { Loading } from "../loading/loading";
+import { formatDate, formatDateAsDMY, formatHourAsHM } from "@/utils/date-formats";
+import { useContext, useState } from "react";
+import { InvoiceDetailsModal } from "./invoice-details-modal";
+import { ConfigContext } from "@/contexts/config-context";
+import { GoAlert, GoArrowRight, GoArrowSmallRight } from "react-icons/go";
+import Link from "next/link";
+
+
+interface InvoiceSearchTableProps {
+  records?:  any;
+  isLoading?: boolean;
+}
+
+export function InvoiceSearchTable(props: InvoiceSearchTableProps) {
+  const { records, isLoading } = props;
+  const [showInvoiceModal, setShowInvoiceModal] = useState<boolean>(false);
+  const [recordSelect, setRecordSelect] = useState<string>("");
+  const { systemInformation } = useContext(ConfigContext);
+
+
+
+  if (isLoading) return <Loading />;
+  if (!records.data) return <NothingHere width="164" height="98" />;
+  if (records.data.length == 0) return <NothingHere text="No se encontraron datos" width="164" height="98" />;
+
+
+
+  const listItems = records.data.map((record: any, key: any) => (
+    <tr key={key} className="border-b">
+      <td className="py-2 px-6 truncate">{ formatDateAsDMY(record?.charged_at) } | { formatHourAsHM(record?.charged_at)} </td>
+      <td className="py-2 px-6 font-bold clickeable" onClick={()=>{ setRecordSelect(record?.id); setShowInvoiceModal(true)}}>{ record?.invoice_assigned?.name } </td>
+      <td className="py-2 px-6">{ record?.invoice }</td>
+      <td className="py-2 px-6">{ record?.client?.name ? record?.client?.name : "N/A" }</td>
+      <td className="py-2 px-6">{ getPaymentTypeName(record?.payment_type) }</td>
+      <td className="py-2 px-6">{ record?.casheir?.name } </td>
+      <td className="py-2 px-6">{ numberToMoney(record?.total ? record?.total : 0, systemInformation) }</td>
+      <td className="py-2 px-6"><Link href={`/invoices/search/${record?.id}`}><GoArrowRight size={20} color="green" /></Link></td>
+    </tr>
+  ));
+
+  return (<div>
+  <div className="w-full overflow-auto">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" className="py-3 px-4 border">Fecha</th>
+          <th scope="col" className="py-3 px-4 border">Tipo</th>
+          <th scope="col" className="py-3 px-4 border">Numero</th>
+          <th scope="col" className="py-3 px-4 border">Cliente</th>
+          <th scope="col" className="py-3 px-4 border">Tipo Pago</th>
+          <th scope="col" className="py-3 px-4 border">Cajero</th>
+          <th scope="col" className="py-3 px-4 border">Total</th>
+          <th scope="col" className="py-3 px-4 border">Ir</th>
+        </tr>
+      </thead>
+      <tbody>{listItems}</tbody>
+    </table>
+    <InvoiceDetailsModal isShow={showInvoiceModal} onClose={()=>setShowInvoiceModal(false)} record={recordSelect} onElectronic={true} />
+ </div>
+ </div>);
+}
