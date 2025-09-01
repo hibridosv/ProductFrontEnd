d2b77b5b6bc61de61e9db240048a7267ffaecac8
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2025-08-31 23:32:21 -0600

    fix(reports): Add missing key to download list
    Add a unique `key` prop to the anchor tag when mapping over the list of downloadable VAT annexes.
    
    This resolves the React warning about missing keys for list items, ensuring efficient DOM updates and preventing potential rendering bugs.

--- src/app/reports/anexos-iva/page.tsx
+++ src/app/reports/anexos-iva/page.tsx
@@ -50,9 +50,9 @@ export default function Page() {
             {
               downloads.data && downloads.data.length > 0 ?
               
-              <ul className="mt-4 border-t border-teal-700">
+              <ul className="mt-4 border-t border-teal-700" >
                 {downloads.data.map((download: any) => (
-                    <a href={`${API_URL}zip/download/${download?.id}`} target="_blank" title="Descargar" >
+                    <a href={`${API_URL}zip/download/${download?.id}`} target="_blank" title="Descargar" key={download?.id}>
                     <li className="flex justify-between p-3 hover:bg-blue-200 hover:text-blue-800">
                         {download.comments}
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24"
